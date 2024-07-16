02db12e70101140380dfdad2d0c6ba8caa9bdaa6
Samara Alves Gomes <saalvesgomes11@gmail.com>
2024-07-16 19:35:20 -0300

    Sobre nós
    Parte javascript feito

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,17 @@ botao.addEventListener("click", () => {
     elementoPlataformas.classList.toggle("ativo");
 });
 
+document.getElementById('toggleButton1').addEventListener('click', function() {
+    var content = document.querySelector('.ocult_btn_topicos1');
+    content.classList.toggle('ocult_btn_topicos1_visible');
+});
+
+document.getElementById('toggleButton2').addEventListener('click', function() {
+    var content = document.querySelector('.ocult_btn_topicos2');
+    content.classList.toggle('ocult_btn_topicos2_visible');
+});
+
+document.getElementById('toggleButton3').addEventListener('click', function() {
+    var content = document.querySelector('.ocult_btn_topicos3');
+    content.classList.toggle('ocult_btn_topicos3_visible');
+});
